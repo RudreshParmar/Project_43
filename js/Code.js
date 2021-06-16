5bf6e667176ceaaf39217b8fae349a04d1759ece
RudreshParmar <75979481+RudreshParmar@users.noreply.github.com>
2021-06-16 09:02:35 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+
+    fill("Yellow")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("Yellow")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("Yellow")
+    textSize(15)
+    text("A T A D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all the information !!", 100,270)
+  }
